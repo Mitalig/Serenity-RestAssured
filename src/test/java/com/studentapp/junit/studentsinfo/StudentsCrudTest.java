b30fc4364d09495f660ecf6b778e81543062163d
Ashwin <ashu271989@gmail.com>
2017-12-28 10:25:57 +0530

    Asserting for the added student

--- src/test/java/com/studentapp/junit/studentsinfo/StudentsCrudTest.java
+++ src/test/java/com/studentapp/junit/studentsinfo/StudentsCrudTest.java
@@ -1,9 +1,15 @@
 package com.studentapp.junit.studentsinfo;
 
+import java.util.HashMap;
+
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import com.studentapp.testbase.TestBase;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit.runners.SerenityRunner;
@@ -11,14 +17,15 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Title;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SerenityRunner.class)
 public class StudentsCrudTest extends TestBase {
 	
 	@Title("Adding a Student using Post Request")
 	@Test
-	public void addStudent ()  {
+	public void test001 ()  {
 		
-		String student = "{\"firstName\":\"Ashwin1\",\"lastName\":\"Harper\",\"email\":\"ashwin3.rhoncus1.Proin@massaQuisqueporttitor.org\",\"programme\":\"Financial Analysis\",\"courses\":[\"Accounting\",\"Statistics\"]}";
+		String student = "{\"firstName\":\"Ashwin2\",\"lastName\":\"Harper\",\"email\":\"ashwin4.rhoncus1.Proin@massaQuisqueporttitor.org\",\"programme\":\"Financial Analysis\",\"courses\":[\"Accounting\",\"Statistics\"]}";
 		
 		 SerenityRest.given()
 		.contentType(ContentType.JSON)
@@ -27,6 +34,22 @@ public class StudentsCrudTest extends TestBase {
 		.post()
 		.then().statusCode(201);
 		
+	}
+	
+	@Title("Checking whether added Student is present")
+	@Test
+	public void test002 ()  {
+		
+		
+	HashMap<String,Object> values= SerenityRest.rest().given()
+			.when()
+			.get("/list")
+			.then()
+			.extract()
+			.path("findAll{it.firstName=='Ashwin2'}.get(0)");
+	
+	System.out.println("The values are "+values);
+	assertThat(values,hasValue("Ashwin2"));
 		
 	}
 
