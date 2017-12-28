5cebd33127a7d1d200cb080ce7543c0c94daa4cb
Ashwin <ashu271989@gmail.com>
2017-12-28 18:31:57 +0530

    Updating the added student

--- src/test/java/com/studentapp/junit/studentsinfo/StudentsCrudTest.java
+++ src/test/java/com/studentapp/junit/studentsinfo/StudentsCrudTest.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 import com.studentapp.testbase.TestBase;
+import com.studentapp.utils.TestUtils;
+
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -20,12 +22,14 @@ import net.thucydides.core.annotations.Title;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SerenityRunner.class)
 public class StudentsCrudTest extends TestBase {
+	public static String firstName= "Ashwin"+TestUtils.getRandomValue();
+	
 	
 	@Title("Adding a Student using Post Request")
 	@Test
 	public void test001 ()  {
 		
-		String student = "{\"firstName\":\"Ashwin2\",\"lastName\":\"Harper\",\"email\":\"ashwin4.rhoncus1.Proin@massaQuisqueporttitor.org\",\"programme\":\"Financial Analysis\",\"courses\":[\"Accounting\",\"Statistics\"]}";
+		String student = "{\"firstName\":\"Ashwin2\",\"lastName\":\"Harper\",\"email\":\"ashwin.rhoncus.Proin@massaQuisqueporttitor.org\",\"programme\":\"Financial Analysis\",\"courses\":[\"Accounting\",\"Statistics\"]}";
 		
 		 SerenityRest.given()
 		.contentType(ContentType.JSON)
@@ -41,6 +45,7 @@ public class StudentsCrudTest extends TestBase {
 	public void test002 ()  {
 		
 		
+		
 	HashMap<String,Object> values= SerenityRest.rest().given()
 			.when()
 			.get("/list")
@@ -50,6 +55,22 @@ public class StudentsCrudTest extends TestBase {
 	
 	System.out.println("The values are "+values);
 	assertThat(values,hasValue("Ashwin2"));
+		
+	}
+	
+	
+	@Title("Updating the added Student")
+	@Test
+	public void test003 ()  {
+		
+	String student = "{\"firstName\":\"Ashwin1_Updated\",\"lastName\":\"Harper\",\"email\":\"ashwin15.rhoncus.Proin@massaQuisqueporttitor.org\",\"programme\":\"Financial Analysis\",\"courses\":[\"Accounting\",\"Statistics\"]}";
+		
+		
+	SerenityRest.rest().given().contentType(ContentType.JSON)
+			.when()
+			.body(student)
+			.put("/105");
+	
 		
 	}
 
